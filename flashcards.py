f57f2308009adc3335e3e78af79589e24dc1a97a
leo ouyang <Leo Ouyang>
2023-02-18 04:31:42 -0500

    Updates

--- flashcards.py
+++
@@ -1,6 +0,0 @@
-import re
-
-filename = input("Give me flashcards file path\n")
-with open(filename,"r",encoding="utf-8") as my_file:
-	x = re.split("-.*\n\n",my_file.read())
-	print(x)
--- src/read.js
+++
@@ -1,7 +0,0 @@
-
-export function read(filename)
-{
-	var file = new File([],filename);
-	var fr=new FileReader();
-	fr.readAsText(file);
-}
