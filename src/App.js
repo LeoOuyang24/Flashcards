478f91e6ee3e905e89f1a221a2a1934200071678
leo ouyang <Leo Ouyang>
2023-07-15 19:30:51 -0400

    all hsk levels added, write game removed, can now choose to do all terms in a set

--- src/App.js
+++ src/App.js
@@ -6,26 +6,48 @@ import {FlashcardsGame} from './flashcards'
 import {WriteGame} from './writeGame'
 
 
-function FirstScreen(props)
+function MenuOption(props)
 {
+  return ( 
+        <div className="option">
+          <input type = "checkbox" name={props.name} title={props.title}/>
+          <br/>
+          {props.message}
+        </div> 
+        )
+}
+
+function FirstScreen(props) //home page
+{
+  let levels = []
+  for (let i = 1; i <= 6; ++i)
+  {
+    levels.push(<MenuOption name={"HSK" + i} message={"HSK" + i}/>)
+  }
   return (
   <form onSubmit={props.handleSubmit}>
     <label for="cardNumber"> How many flashcards to study?
-      <input type="number" min="5" name="amount"/>
+      <input type="number" min="5" name="amount" id="numOfCards" disabled={false}/>
      </label>
+     All Terms
+     <input type="checkbox" name="all" title="Study all terms" onClick={() => {
+      let input = document.getElementById("numOfCards")
+      input.disabled = !input.disabled
+    }}/>
      <br/>
      <br/>
     <label for="startIndex">Flashcard to start at:
       <input type="number" min="1" name="index"/>
     </label>
       <br/>
-      <br/>
-    <label> Write
-      <input type = "checkbox" name="write"/>
-    </label>
-    <label> Randomize?
-      <input type = "checkbox" name="randomize"/>
-    </label>
+      <div className="optionsMenu">
+        {levels}
+      </div>
+      <div className="optionsMenu">
+        {/*<MenuOption name="write" title="Write something using HSK terms" message="Write"/> uncomment this to enable write game*/}
+        <MenuOption name="randomize" title="Shuffle the order of the flashcards" message="Randomize?"/>
+        <MenuOption name="charOnly" title="Study individual characterse only" message="Characters Only"/>
+      </div>
       <br/>
       <br/>
       <input type="submit"/>
@@ -41,7 +63,6 @@ export class App extends React.Component
     this.state = {
             cardAmount: 10,
             startIndex: 1,
-            link:"https://raw.githubusercontent.com/clem109/hsk-vocabulary/master/hsk-vocab-json/hsk-level-4.json",
             game: 0} //0 = first screen, 1 = flashcards, 2 = write
     this.handleSubmit = this.handleSubmit.bind(this);
 
@@ -49,17 +70,35 @@ export class App extends React.Component
   handleSubmit(event)
   {
     event.preventDefault()
-    this.setState({cardAmount: event.target.amount.value ? parseInt(event.target.amount.value) : 10})
+
+    this.setState({cardAmount: event.target.amount.value ? parseInt(event.target.amount.value) : event.target.all.checked ? -1 : 10}) //do the value provided, or -1 (all flashcards) if "all cards" was checked, or 10 if no input at all
     this.setState({startIndex: event.target.index.value ? parseInt(event.target.index.value) : 1})
     this.setState({shuffle: event.target.randomize.checked })
-    this.setState({game: event.target.write.checked ? 2 : 1})
+    this.setState({game: 1})
+    //this.setState({game: event.target.write.checked ? 2 : 1}) <-- uncomment for write game
+    this.setState({charOnly: event.target.charOnly.checked})
+    let levels = []
+
+    for (let i =1; i <= 6; ++i)
+    {
+      if (event.target["HSK" + i].checked)
+      {
+        levels.push(i)
+      }
+    }
+    this.setState({levels: levels })
   }
   render()
   {
     let game = undefined
     if (this.state.game == 1)
     {
-      game = <FlashcardsGame link={this.state.link} cardAmount={this.state.cardAmount} startIndex = {this.state.startIndex} shuffle = {this.state.shuffle}/>
+      game = <FlashcardsGame
+      cardAmount={this.state.cardAmount} 
+      startIndex = {this.state.startIndex} 
+      shuffle = {this.state.shuffle}
+      charOnly = {this.state.charOnly}
+      levels = {this.state.levels}/>
     }
     else if (this.state.game == 2)
     {
--- src/term.js
+++ src/term.js
@@ -1,6 +1,6 @@
 import React, { Component, useState, useEffect } from 'react';
 
-export function Text(props){
+export function Text(props){ //text bubble, used to show a term or a definition
 	return (
 	<div className = "term" style={{backgroundColor: props.color ? props.color : "white"}}>
 		{props.text}
@@ -8,12 +8,41 @@ export function Text(props){
 
 }
 
-export function Term(props){
-	let texts = (props.term ?  (props.showTerm ? <Text text = {props.term.hanzi}/> : 
-				props.term.translations.reduce((accum,value) => (accum.length < 6 ? accum.concat([<Text text = {value}/>]) : accum),[<Text text = {props.term.pinyin} color = "yellow"/>])) : "UNKNOWN")
+/*export function Term(props){ //the actual flashcard
+	let texts = (props.term ?  //if the term exists...
+					(props.showTerm ?  //if we are supposed to show it...
+						<Text text = {props.term.hanzi}/> : //show the term
+						props.term.translations.reduce((accum,value) => (accum.length < 6 ? //...otherwise show the first 6 definitions
+							accum.concat([<Text text = {value}/>]) : 
+							accum),
+							[<Text text = {props.term.pinyin} color = "yellow"/>])) : //highlight the pinyin and attach it as the top most text bubble
+					"UNKNOWN") //term couldn't be found for some reason
 	return (
 			<div>
 				{texts}
 			</div>
 		)
+}*/
+
+export class Term extends React.Component {
+	constructor(props)
+	{
+		super(props)
+	}
+	render()
+	{
+		let texts = (this.props.term ?  //if the term exists...
+					(this.props.showTerm ?  //if we are supposed to show it...
+						<Text text = {this.props.term.front}/> : //show the term
+						this.props.term.back.reduce((accum,value) => (accum.length < 6 ? //...otherwise show the first 6 definitions
+							accum.concat([<Text text = {value}/>]) : 
+							accum),
+							[<Text text = {this.props.term.highlight} color = "yellow"/>])) : //highlight the pinyin and attach it as the top most text bubble
+					"UNKNOWN") //term couldn't be found for some reason
+	return (
+			<div>
+				{texts}
+			</div>
+		)
+	}
 }
